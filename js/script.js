08d3efa4d9364f763d87b5d105dc80431563b4d9
Thomas <gonzo113@bassfreaks.de>
2021-01-10 00:58:13 +0100

    fixed position of svg to bottom (xMidYMax meet); wrote first javascript for opening box with information

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 window.onload = function(){
 
+    //var allLines = document.querySelectorAll('.lines');
+    var allDots = document.querySelectorAll('.dots');
+    var showcase = document.getElementById('showcase');
 
+    showcase.addEventListener("click",function(event){
+        showcase.style.display = "none";
+    });
+
+    allDots.forEach((item)=>{
+        item.addEventListener("click",function(event){
+            show(event);
+        });
+    });
+
+    function show(clicked){
+        showcase.offsetTop = clicked.target.offsetTop;
+        showcase.offsetLeft = clicked.target.offsetLeft;
+        showcase.innerText = clicked.target.id;
+        console.log(clicked.target);
+        showcase.innerHTML.fontcolor(clicked.target.style.fill);
+        showcase.style.display = "block";
+
+    }
 }
